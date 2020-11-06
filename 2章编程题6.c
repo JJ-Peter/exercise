d3dbb9a89739419a6b674e471d5ee27fd1d51561
JJ-Peter <1519108798@qq.com>
2020-11-06 20:11:29 +0800

    编程练习

---
+++ 2章编程题6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+	int x;
+	printf("Enter the value of x = ");
+	scanf("%d",&x);
+	int y ;
+	y = ((((3*x+2)*x-5)*x-1)*x+7)*x-6; 
+	printf("((((3*x+2)*x-5)*x-1)*x+7)*x-6=%d",y);
+	return 0;
+ } 
