ebb441226efb8cdd1976d95450255cb59abf8e32
JJ-Peter <1519108798@qq.com>
2020-11-10 15:22:54 +0800

    编程练习

---
+++ 2个整数相除.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{ 
+    int a, b, m; 
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	m = a / b;
+	printf("m=%d",m);
+    return 0;
+} 
