3c49ffc5f67e674b6a9fd166b760ed3ed96bc510
JJ-Peter <1519108798@qq.com>
2020-11-21 16:59:08 +0800

    在任意正输入数中显示最大正数

---
+++ 7章编程题1.c
@@ -0,0 +1,15 @@
+//在任意正输入数中显示最大正数 
+#include<stdio.h>
+int main()
+{
+	printf("Hello!\nYou are supposed to enter a number( > 0 )\nIf your number <= 0, the largest number will be shown\n\n");
+	float x, k;
+	k = 0;	
+	do{
+	printf("Enter a number: ");
+	scanf("%f",&x);	
+	k =	x > k ? x : k;
+	}while(x>0);
+	printf("The largest number entered was %g\n",k);
+	return 0;
+}
